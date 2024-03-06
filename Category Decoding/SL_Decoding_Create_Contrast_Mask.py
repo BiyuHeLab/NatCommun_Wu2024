0bd30b9158204b1aed71146bf0349c7c2b031fe6
Yuan-hao Wu <yuanhaowu@gmail.com>
2024-03-05 23:10:43 -0500

    Add explanations and comments

--- Category Decoding/SL_Decoding_Create_Contrast_Mask.py
+++ Category Decoding/SL_Decoding_Create_Contrast_Mask.py
@@ -3,12 +3,21 @@
 """
 Created on Tue Sep 26 17:49:25 2023
 
-Contrast decoding accuracy maps against chance level and generate a thresholding
-mask.
-Decoding accuracies were derived from from high and low prestimulus activity
-trials grouped by prestimulus BOLD signals in four ROIs.   
-
-
+-   Compare Category decoding accuracy derived high and low presitmulus activity trials
+    against chance level across the entire brain, respectively
+-   Create thresholding mask including voxels showing above-decoding accuracy.
+-   required inputs:
+    Whole-brain decoding accuracy maps from following conditions:
+    High visual network prestimulus activity trials
+    High CO network prestimulus activity trials
+    High vmPFC prestimulus activity trials
+    High RSC prestimulus activity trials
+    Low visual network prestimulus activity trials
+    Low CO network prestimulus activity trials
+    Low vmPFC prestimulus activity trials
+    Low RSC prestimulus activity trials
+-   Output a threshold mask for each of the conditions above.     
+ 
 @author: wuy19
 """
 import HLTP
@@ -18,23 +27,25 @@ import nibabel
 from nilearn.image import index_img, new_img_like, binarize_img, smooth_img, concat_imgs
 from nilearn.input_data import NiftiMasker
 
+# function for transforming p-value to z-score
 def z_score(pvalue):
     pvalue = np.minimum(np.maximum(pvalue, 1.e-300), 1. - 1.e-16)
     return scipy.stats.norm.isf(pvalue)
 
-
-
 ProjDir = "/isilon/LFMI/VMdrive/data/HLTP_fMRI/"
 OutDir = HLTP.group_result + "/nilearn_sl/"
 
-
+# import the standard brain mask
 mask_fname ='/usr/local/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz'
 mask_img = nibabel.load(mask_fname)
 
 
 Seeds = ['CO', 'visual', 'vmPFC', 'RSC']
 
+#Iterate across the four seed ROIs 
 for seed in Seeds:
+    # Load each subject's decoding accuracy maps from high and low 
+    # presitmulus activity trials, and smooth them.
     filesH = []; filesL = []; 
     for sub in HLTP.subjects:
         SubDir = ProjDir + "sub" + str(sub).zfill(2) + "/proc_data/func/"
@@ -48,7 +59,8 @@ for seed in Seeds:
     img4H_smoothed,img4L_smoothed  = smooth_img(img4H, 3), smooth_img(img4L, 3)
     del img4H, img4L, filesH, filesL
     
-    
+    # Exclude voxels outside the standard brain mask and contrast the included
+    # voxels against chance level accuracy. outputs: t and p-maps 
     nifti_masker = NiftiMasker(mask_img = mask_img)
     for level in ['high', 'low']:
         if level =="high":           
@@ -68,7 +80,8 @@ for seed in Seeds:
         z_fname = OutDir + '/SL_cross_' + level + '_' + seed + '_AgainstChance_3fwhm_zstat.nii.gz'
         z_img.to_filename(z_fname)
         
-        # make contrast mask at z = 1.64
+        # generate a mask that includes voxels with above chace-level accuracy at p < 0.05 (z > 1.64)
+        # and save it. 
         con_mask = (z_map > 1.64).astype(int)
         con_mask = new_img_like(mask_img, con_mask)  
         con_mask.to_filename(OutDir + '/SL_cross_' + level + '_' + seed + '_ThresholdingMask.nii.gz')
